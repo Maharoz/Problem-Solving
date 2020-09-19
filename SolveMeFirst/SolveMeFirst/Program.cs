68fbf8283befa96441e3e5fd709e9d86756fbe9a
Maharoz <mahruj66@gmail.com>
2020-09-19 13:01:53 +0600

    first commit

---
+++ SolveMeFirst/SolveMeFirst/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SolveMeFirst
+{
+    class Program
+    {
+       
+            //Problem Link -->https://www.hackerrank.com/challenges/solve-me-first/problem
+
+             static int solveMeFirst(int a, int b)
+            {
+                return a + b;
+            }
+            static void Main(string[] args)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(solveMeFirst(a, b));
+
+           
+        }
+    }
+}
