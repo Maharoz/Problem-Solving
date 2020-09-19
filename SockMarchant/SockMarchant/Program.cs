a4c7643eec8728e3ff7c365176441f7513bf16b3
Maharoz <mahruj66@gmail.com>
2020-09-19 13:13:04 +0600

    Sock marchant solved

---
+++ SockMarchant/SockMarchant/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace SockMarchant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //problem link-->https://www.hackerrank.com/challenges/sock-merchant/problem
+            int n = int.Parse(Console.ReadLine());
+            string[] c = Console.ReadLine().Split(' ');
+            int result = 0;
+            foreach(string color in c.Distinct())
+            {
+                result += c.Where(ci => ci == color).Count() / 2;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
