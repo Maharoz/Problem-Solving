cbd293fe7441bb0eed5468c3462134a3a8b253e5
Maharoz <mahruj66@gmail.com>
2020-09-20 19:12:28 +0600

    First non repeating character solved by dictionary

---
+++ FirstNonRepeatingCharacter/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CharFinder f = new CharFinder();
+            var result=f.findFirstNonRepeatingChar("a green apple");
+            Console.WriteLine(result);
+        }
+    }
+
+    public class CharFinder
+    {
+        public char findFirstNonRepeatingChar(string str)
+        {
+            Dictionary<string,int> dictionarycharacter = new Dictionary<string,int>();
+
+            foreach (char s in str)
+            {
+                if (dictionarycharacter.ContainsKey(s.ToString()))
+                {
+                    var count = dictionarycharacter[s.ToString()];
+                    dictionarycharacter[s.ToString()] = count+1;
+                }
+                else
+                {
+                    dictionarycharacter[s.ToString()] = 1;
+                }
+            }
+
+            foreach (char s in str)
+            {
+                if(dictionarycharacter[s.ToString()]==1)
+                {
+                    return s;
+                }
+            }
+
+            return char.MinValue;
+        }
+        
+    }
+}
