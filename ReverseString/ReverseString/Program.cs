5471fae2869b90f1f835c7cd3457466d09e7c6c5
Maharoz <mahruj66@gmail.com>
2020-09-23 22:04:59 +0600

    String reverse added

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "abcd";
+            StringReverse r = new StringReverse();
+            string result = r.reverse(str);
+
+            Console.WriteLine(result);
+        }
+    }
+
+    public class StringReverse
+    {
+        public string reverse(string input)
+        {
+            Stack MyStack = new Stack();
+
+            foreach(char a in input)
+            {
+                MyStack.Push(a);
+            }
+
+            string reversed = string.Empty;
+
+            while (MyStack.Count != 0)
+            {
+                reversed += MyStack.Pop();
+            }
+
+            return reversed;
+        }
+    }
+}
