9a7d3b585885dd3b8483b40d54306579c39f8d3e
Maharoz <mahruj66@gmail.com>
2021-01-06 12:27:37 +0600

    Circle in square solved

---
+++ LOJ1022/LOJ1022/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LOJ1022
+{
+    class Program
+    {
+        //problem url https://lightoj.com/problem/circle-in-square
+        static void Main(string[] args)
+        {
+            int i = 1, n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i < n + 1; i++)
+            {
+                double radius = Convert.ToDouble(Console.ReadLine());
+                double areaOfSquare = (radius + radius)* (radius + radius);
+                double areOfCircle = 3.1416 * radius* radius;
+                double shadedArea = areaOfSquare - areOfCircle;
+                double result = RoundUp(shadedArea, 2);
+                Console.WriteLine("Case" + i +":"+result);
+            }
+        }
+        public static double RoundUp(double input, int places)
+        {
+            double multiplier = Math.Pow(10, Convert.ToDouble(places));
+            return Math.Ceiling(input * multiplier) / multiplier;
+        }
+    }
+}
