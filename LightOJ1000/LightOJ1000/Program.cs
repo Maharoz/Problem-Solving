477ed92ca18b16cac67d053f21a6b988f147b062
Maharoz <mahruj66@gmail.com>
2021-01-06 10:59:02 +0600

    Greeting from lightoj solved

---
+++ LightOJ1000/LightOJ1000/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LightOJ1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Problem URL
+            int n, i = 1, x, y;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i < n +1; i++)
+            {
+                string[] number = Console.ReadLine().Split();
+                int total = Convert.ToInt32(number[0]) + Convert.ToInt32(number[1]);
+                Console.WriteLine(" Case " + i +":"+ total);
+                
+            }
+        }
+    }
+}
