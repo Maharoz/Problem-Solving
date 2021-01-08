d040317e8f83325ce15567e844afc6538474517a
Maharoz <mahruj66@gmail.com>
2021-01-08 20:26:59 +0600

    duration solved

---
+++ Doctor's Secret/Doctor's Secret/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Doctor_s_Secret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string r1;
+            string[] a = Console.ReadLine().Split();
+            int length = Convert.ToInt32(a[0]);
+            int page = Convert.ToInt32(a[1]);
+
+            if (length <= 23)
+            {
+                 r1 = "Take";
+            }
+            else
+            {
+                 r1 = "Don't take";
+            }
+
+            Console.WriteLine(r1 + " Medicine");
+        }
+    }
+}
---
+++ Duration/Duration/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Duration
+{
+    class Program
+    {
+        //Problem link-->https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/duration/
+        static void Main(string[] args)
+        {
+            int noOfInput,a=0,b=0;
+            noOfInput = Convert.ToInt32(Console.ReadLine());
+           
+
+            for(int i = 0; i < noOfInput; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int min1 = Convert.ToInt32(input[0]) * 60 + Convert.ToInt32(input[1]);
+                int min2 = Convert.ToInt32(input[2]) * 60 + Convert.ToInt32(input[3]);
+                min2 -= min1;
+                int hours = min2 / 60;
+               int min = min2 % 60;
+                Console.WriteLine(hours + " " + min);
+            }
+
+
+
+
+        }
+    }
+}
