76fe91c32f6dacdac3ca5fd9383a7e2bf9ac35a1
Maharoz <mahruj66@gmail.com>
2021-01-09 13:35:02 +0600

    Mini max solved

---
+++ MiniMax/MiniMax/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace MiniMax
+{
+    class Program
+    {
+        //problem URL-->https://www.hackerrank.com/challenges/mini-max-sum/problem
+        static void Main(string[] args)
+        {
+
+            int[] input = new int[5];
+            string[] arrayVal = Console.ReadLine().Split();
+            int[] myInts = Array.ConvertAll(arrayVal, s => int.Parse(s));
+            int smallestSum=0, bisggestSum=0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = myInts[i];
+            }
+
+            int max = input.Max();
+            int min = input.Min();
+
+            for(int j =0;j< input.Length; j++)
+            {
+                if (input[j] != max)
+                {
+                    bisggestSum = bisggestSum + input[j];
+                }
+                if(input[j] != min)
+                {
+                    smallestSum = smallestSum + input[j];
+                }
+            }
+
+            Console.WriteLine(bisggestSum + " " + smallestSum);
+        }
+    }
+}
