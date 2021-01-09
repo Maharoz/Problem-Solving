812ad9b3f5b9e6f16c5fdd141d21f82642fa375f
Maharoz <mahruj66@gmail.com>
2021-01-09 14:53:28 +0600

    Bill division solved

---
+++ BillDivision/BillDivision/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace BillDivision
+{
+    class Program
+    {
+
+        //Problem Url-->https://www.hackerrank.com/challenges/bon-appetit/problem
+        static void Main(string[] args)
+        {
+            string[] initialInput = Console.ReadLine().Split();
+            int noOfItem = Convert.ToInt32(initialInput[0]);
+            int annaDidntEat = Convert.ToInt32(initialInput[1]);
+
+            string[] item = Console.ReadLine().Split();
+            int[] orderedItem = Array.ConvertAll(item, s => int.Parse(s));
+            int annaPaid = Convert.ToInt32(Console.ReadLine());
+
+            int annaHaveToPay = 0;
+
+            for(int i = 0;i< orderedItem.Length; i++)
+            {
+                if(i!= annaDidntEat)
+                {
+                    annaHaveToPay = annaHaveToPay + orderedItem[i];
+                }
+            }
+            annaHaveToPay = annaHaveToPay / 2;
+            if (annaHaveToPay == annaPaid)
+            {
+                Console.WriteLine("Bon Appetit");
+            }else if (annaPaid> annaHaveToPay)
+            {
+                Console.WriteLine(annaPaid- annaHaveToPay);
+            }
+        }
+    }
+}
