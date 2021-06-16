d89dcdce870f37bc8948e01ca238037e20af3728
Maharoz <mahruj66@gmail.com>
2021-06-16 11:32:05 +0600

    Simple array sum

---
+++ SimpleArraySum/SimpleArraySum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        //Problem URL --> https://www.hackerrank.com/challenges/simple-array-sum/problem
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] c = Console.ReadLine().Split(' ');
+
+            int result = 0;
+            foreach (var item in c)
+            {
+                result += Convert.ToInt32(item);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
