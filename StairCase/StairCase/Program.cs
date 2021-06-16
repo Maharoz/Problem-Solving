1f61130ea811bbdda756827ac394b342793e2b09
Maharoz <mahruj66@gmail.com>
2021-06-16 11:43:08 +0600

    Stair Case

---
+++ StairCase/StairCase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace StairCase
+{
+    class Program
+    {
+        //Problem URL-->https://www.hackerrank.com/challenges/staircase/submissions/code/123555606
+        static void Main(string[] args)
+        {
+            var n = int.Parse(ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                var spaces = new String(' ', n - i);
+                var hashes = new String('#', i);
+                WriteLine(spaces + hashes);
+
+            }
+
+        }
+    }
+}
