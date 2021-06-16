94b7ecb50a52df0efdcde665f7d8829b4b9fe139
Maharoz <mahruj66@gmail.com>
2021-06-16 11:37:46 +0600

    A vERY BIG SUM

---
+++ AVeryBigSum/AVeryBigSum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVeryBigSum
+{
+    class Program
+    {
+
+        //Problem URL-->https://www.hackerrank.com/challenges/a-very-big-sum/submissions/code/123496869
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] c = Console.ReadLine().Split(' ');
+            long result = 0;
+            var ar = Array.ConvertAll(c, long.Parse);
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                result += ar[i];
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
