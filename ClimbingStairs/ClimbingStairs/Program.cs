742902c4d8baa99f0f66f2eeacbffe21c72d89f8
Maharoz <mahruj66@gmail.com>
2021-06-30 11:02:13 +0600

    Fibonacci series added

---
+++ ClimbingStairs/ClimbingStairs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ClimbingStairs
+{
+    class Program
+    {
+
+        //Problem URL-->https://leetcode.com/problems/climbing-stairs/solution/
+        static void Main(string[] args)
+        {
+            int s = 4, m = 2;
+            Console.Write("Number of ways = "
+                          + countWays(s, m));
+
+
+        }
+        static int countWaysUtil(int n, int m)
+        {
+            if (n <= 1)
+                return n;
+            int res = 0;
+
+            for (int i = 1; i <= m && i <= n; i++)
+                res += countWaysUtil(n - i, m);
+            return res;
+        }
+
+        // Returns number of ways to reach
+        // s'th stair
+        static int countWays(int s, int m)
+        {
+            return countWaysUtil(s + 1, m);
+        }
+
+
+    }
+}
---
+++ FibonacciPosition/FibonacciPosition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciPosition
+{
+    class Program
+    {
+
+        //Problem definition
+        static void Main(string[] args)
+        {
+            int n = 9;
+            Console.WriteLine(fib(n));
+        }
+
+        static int fib(int n)
+        {
+            int[] f = new int[n + 2];
+            int i;
+
+            f[0]=0;
+            f[1] = 1;
+
+            for (i = 2; i <= n; i++)
+            {
+                f[i] = f[i - 1] + f[i - 2];
+            }
+
+            return f[n];
+        }
+    }
+}
