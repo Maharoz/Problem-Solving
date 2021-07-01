5b63af8522df2ac5e53013a347c4f3f8b01e6a12
Maharoz <mahruj66@gmail.com>
2021-07-02 00:32:54 +0600

    container with most water

---
+++ ContainerWithMostWater/ContainerWithMostWater/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerWithMostWater
+{
+
+    //https://leetcode.com/problems/container-with-most-water/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 5, 4, 3 };
+
+            int len1 = 4;
+            Console.WriteLine(maxArea(a, len1));
+
+        }
+
+        public static int maxArea(int[] A,int len)
+        {
+            int l = 0;
+            int r = len - 1;
+            int area = 0;
+
+            while (l < r)
+            {
+                area = Math.Max(area, Math.Min(A[l], A[r]) * (r - l));
+
+                if (A[l] < A[r])
+                    l += 1;
+
+                else
+                    r -= 1;
+            }
+            return area;
+        }
+      
+
+    }
+}
