c1dfd2288053ecafc44783bbf2184f69880ca381
Maharoz <mahruj66@gmail.com>
2021-11-02 00:40:41 +0600

    DPS-001 added from code chef

---
+++ DPS001atm/DPS001atm/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DPS001atm
+{
+    class Program
+    {
+        //Problem URL--> https://www.codechef.com/problems/HS08TEST
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int amountToBeWithDraw = Convert.ToInt32(input[0]);
+            double totalBalance = Convert.ToDouble(input[1]);
+
+            if(amountToBeWithDraw % 5 != 0)
+            {
+                Console.WriteLine(totalBalance.ToString("F2"));
+               // return;
+            }
+            else if (amountToBeWithDraw> totalBalance)
+            {
+                Console.WriteLine(totalBalance.ToString("F2"));
+            }
+            else
+            {
+                double result = totalBalance - (amountToBeWithDraw + 0.5);
+                Console.WriteLine(result.ToString("F2"));
+            }
+            
+        }
+    }
+}
