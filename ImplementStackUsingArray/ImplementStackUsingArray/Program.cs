909b40d6f29facd7a84e4251e7b6ca31007ce9a0
Maharoz <mahruj66@gmail.com>
2022-09-11 22:22:32 +0600

    geek for geek balanced paratnthesis

---
+++ ImplementStackUsingArray/ImplementStackUsingArray/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+namespace ImplementStackUsingArray
+{
+
+    //https://practice.geeksforgeeks.org/problems/implement-stack-using-array/1?page=1&category[]=Stack&sortBy=submissions
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string op = Console.ReadLine();
+        }
+    }
+
+
+    class MyStack
+    {
+        //Complete this function
+        int[] arr = { };
+        int i = 0;
+        public MyStack()
+        {
+        }
+        public void push(int x)
+        {
+            arr[i] = x;
+            i = i+1;
+        }
+        public int pop()
+        {
+            int result = arr[i - 1];
+            arr[i - 1] = 0;
+            i = i-1;
+            return result;
+        }
+
+    }
+}
---
+++ ValidParanthesis/ValidParanthesis/Program.cs
@@ -0,0 +1,39 @@
+﻿//https://practice.geeksforgeeks.org/problems/parenthesis-checker2744/1?page=1&difficulty[]=0&category[]=Stack&sortBy=submissions
+
+CheckString("{([])}");
+
+static bool CheckString(string input)
+{
+    if (string.IsNullOrEmpty(input))
+    {
+        return true;
+    }
+
+    Stack<char> brackets = new Stack<char>();
+
+    foreach (var c in input)
+    {
+        if (c == '[' || c == '{' || c == '(')
+        {
+            brackets.Push(c);
+        }
+        else if (c == ']' || c == '}' || c == ')')
+        {
+            if (brackets.Count() <= 0)
+            {
+                return false;
+            }
+            char open = brackets.Pop();
+
+            if (c == '}' && open != '{' ||
+         c == ')' && open != '(' ||
+         c == ']' && open != '[')
+                return false;
+        }
+    }
+
+    if (brackets.Count > 0)
+        return false;
+
+    return true;
+}
