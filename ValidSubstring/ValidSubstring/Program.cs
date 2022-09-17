948f45a0cae0bfa2595d9f6ad22207bb5a521645
Maharoz <mahruj66@gmail.com>
2022-09-17 13:48:26 +0600

    sticker theif solved

--- ValidSubstring/ValidSubstring/Program.cs
+++ ValidSubstring/ValidSubstring/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//https://practice.geeksforgeeks.org/problems/valid-substring0624/1
+
+static int findMax(string s)
+{
+    int n = s.Length;
+
+    Stack<int> stack = new Stack<int>();
+    stack.Push(-1);
+    int result = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '(')
+        {
+            stack.Push(i);
+        }
+        else
+        {
+            if (stack.Count > 0)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count > 0)
+            {
+                result
+                    = Math.Max(result,
+                               i - stack.Peek());
+            }
+
+            else
+            {
+                stack.Push(i);
+            }
+        }
+    }
+    return result;
+
+}
+
+//string str = "((()()";
+
+//// Function call
+//Console.WriteLine(findMax(str));
+
+string str = ")(()))))";
+
+// Function call
+Console.WriteLine(findMax(str));
---
+++ stickler theif/stickler theif/Program.cs
@@ -0,0 +1,33 @@
+﻿//https://practice.geeksforgeeks.org/problems/stickler-theif-1587115621/1?page=1&difficulty[]=0&category[]=Dynamic%20Programming&sortBy=submissions
+
+int[] a = { 5, 5, 10, 20, 100, 5 };
+int x = 6;
+
+FindMaxSum(a, x);
+
+int FindMaxSum(int[] arr, int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return arr[0];
+    }
+    if (n == 2)
+    {
+        return Math.Max(arr[0], arr[1]);
+    }
+
+    int[] b = new int[n];
+    b[0] = arr[0];
+    b[1] = Math.Max(arr[0], arr[1]);
+
+    for (int i = 2; i < n; i++)
+    {
+        b[i] = Math.Max(b[i - 2] + arr[i], b[i - 1]);
+    }
+    return b[n - 1];
+}
+
