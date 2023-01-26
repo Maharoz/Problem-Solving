b1a912284aa791e224b5c335a1a8d7583b817e35
Maharoz <mahruj66@gmail.com>
2023-01-27 00:16:19 +0600

    reorder-a-array-according-to-given-indexes

---
+++ Solved Problems 2023/reorder-the array-according-to-given-index/reorder-the array-according-to-given-index/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace reorder_the_array_according_to_given_index
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = { 10, 11, 12 };
+			int[] index = { 1, 0, 2 };
+			reOrderedArray(arr, index);
+		}
+		static int[] reOrderedArray(int[] array, int[] index)
+		{
+			int[] result = new int[array.Length];	
+
+			for(int i=0; i<array.Length;i++)
+			{
+				int x = index[i];
+				result[x] =array[i];
+			}
+			return result;
+		}
+	}
+}
