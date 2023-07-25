32f53c2d118d56e203d0706456bb34e95fdd9097
Maharoz <mahruj66@gmail.com>
2023-07-25 10:38:06 +0600

    Implement stack using queue

---
+++ Stack/StackEasyProblems/StackEasyProblems/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StackEasyProblems
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			QueueUsingStack q = new QueueUsingStack();
+			q.enQueue(1);
+			q.enQueue(2);
+			q.enQueue(3);
+
+			Console.Write(q.deQueue() + " ");
+			Console.Write(q.deQueue() + " ");
+			Console.Write(q.deQueue());
+		}
+	}
+}
