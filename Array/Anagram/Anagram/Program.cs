6cbd9c5a09ad306f7092aac9cd852818510fce56
Maharoz <mahruj66@gmail.com>
2024-01-19 23:04:45 +0600

    Anagram added

---
+++ Array/Anagram/Anagram/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Anagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            isAnagram("geeksforgeeks", "forgeeksgeeks");
+            Console.WriteLine(isAnagram("geeksforgeeks", "forgeeksgeeks"));
+        }
+
+        public static bool isAnagram(string a, string b)
+        {
+
+            if(a.Length != b.Length)
+            {
+                return false;
+            }
+
+            int[] alphabet = new int[26];
+
+            foreach (char i in a.ToLower())
+            {
+                int index =  i-97;
+                alphabet[index]++;
+            }
+            foreach (char i in b.ToLower())
+            {
+                int index = i-97;
+                alphabet[index]--; 
+            }
+
+            return alphabet.Any(x => x > 0)==false?true:false;
+            //return true;
+        }
+    }
+}
---
+++ Recursion/PrintOneToNWithoutLoop/PrintOneToNWithoutLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PrintOneToNWithoutLoop
+{
+
+    //https://www.geeksforgeeks.org/problems/print-1-to-n-without-using-loops-1587115620/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            printNos(10);
+        }
+
+        public static void printNos(int N)
+        {
+            if(N > 0)
+            {
+                printNos(N-1);
+                Console.Write(N+ " ");
+            }
+          
+        }
+    }
+}
