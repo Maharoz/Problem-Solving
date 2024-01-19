759029433d79d2ec515cfa1117b383713cf30fae
Maharoz <mahruj66@gmail.com>
2024-01-20 00:13:05 +0600

    Majority added added

---
+++ Array/MajorityElement/MajorityElement/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+
+namespace MajorityElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            int[] a = { 15};
+            majorityElement(a, 1);
+
+
+        }
+        public static int majorityElement(int[] a, int size)
+        {
+            // your code here
+            int result = -1;
+
+            int majorityFactor = size / 2;
+
+           Dictionary<int,int> map = new Dictionary<int,int>();
+
+            foreach (int i in a)
+            {
+                if (map.ContainsKey(i))
+                {
+                    map[i]++;
+                }
+                else
+                {
+                    map.Add(i, 1);
+                }
+            }
+
+            int keyWithMaxValue = map.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+            int value = map[keyWithMaxValue];
+            if (value > majorityFactor)
+            {
+                result = keyWithMaxValue;
+            }
+            return result;
+        }
+    }
+}
---
+++ Sliding Window/SubArrayWithGivenSum/SubArrayWithGivenSum/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace SubArrayWithGivenSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] a = { 1, 2, 3, 7, 5 };
+            List<int> ab = new List<int>();
+            ab.Add(1);
+           // ab.Add(2);
+           // ab.Add(3);
+           // ab.Add(7);
+           // ab.Add(5);
+            subarraySum(ab, 0, 0);
+        }
+
+        public static List<int> subarraySum(List<int> arr, int n, long s)
+        {
+            int last = 0;
+            int start = 0;
+            long currsum = 0;
+            bool flag = false;
+            List<int> res = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                currsum += arr[i];
+
+                if (currsum >= s)
+                {
+                    last = i;
+                    while (s < currsum && start < last)
+                    {
+                        currsum -= arr[start];
+                        ++start;
+                    }
+
+                    if (currsum == s)
+                    {
+                        res.Add(start + 1);
+                        res.Add(last + 1);
+
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            //if no subarray is found, we store -1 in result.
+            if (flag == false)
+                res.Add(-1);
+
+            //returning the result.
+            return res;
+        }
+    }
+}
