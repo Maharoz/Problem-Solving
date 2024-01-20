e255babd870a5720d8a171215c9285adb212ad4d
Maharoz <mahruj66@gmail.com>
2024-01-20 11:48:35 +0600

    Majority alement added

--- Array/MajorityElement/MajorityElement/Program.cs
+++ Array/MajorityElement/MajorityElement/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Net.NetworkInformation;
 
 namespace MajorityElement
 {
@@ -7,39 +8,60 @@ namespace MajorityElement
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
-            int[] a = { 15};
-            majorityElement(a, 1);
+            int[] a = { 3,1,3,3,2};
+            majorityElement(a, 5);
 
 
         }
+    
         public static int majorityElement(int[] a, int size)
         {
-            // your code here
-            int result = -1;
+            /* Find the candidate for Majority*/
+            int cand = findCandidate(a, size);
 
-            int majorityFactor = size / 2;
-
-           Dictionary<int,int> map = new Dictionary<int,int>();
-
-            foreach (int i in a)
-            {
-                if (map.ContainsKey(i))
-                {
-                    map[i]++;
-                }
-                else
-                {
-                    map.Add(i, 1);
-                }
-            }
-
-            int keyWithMaxValue = map.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
-            int value = map[keyWithMaxValue];
-            if (value > majorityFactor)
-            {
-                result = keyWithMaxValue;
-            }
-            return result;
+            /* Print the candidate if it is Majority*/
+            if (isMajority(a, size, cand) == 1)
+                return cand;
+            else
+                return -1;
         }
+        public static int findCandidate(int[] a, int size)
+            {
+                int maj_index = 0, count = 1;
+
+                // use linear search to compute candidate for majority element
+                for (int i = 1; i < size; i++)
+                {
+
+                    if (a[maj_index] == a[i])
+                        count++;
+                    else
+                        count--;
+                    if (count == 0)
+                    {
+                        maj_index = i;
+                        count = 1;
+                    }
+                }
+                return a[maj_index];
+            }
+
+
+        /* Function to check if the candidate 
+           occurs more than n/2 times */
+        public static int isMajority(int[] a, int size, int cand)
+        {
+            int count = 0;
+            for (int i = 0; i < size; i++)
+                if (a[i] == cand)
+                    count++;
+
+            if (count > size / 2)
+                return 1;
+
+            else
+                return 0;
+        }
+
     }
 }
